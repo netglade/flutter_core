dbe1b2de9555bda96f185b3c82bc999e6ca3231e
Honza Bittner <git@tenhobi.dev>
2022-11-15 17:24:25 +0100

    Add lints and code_metrics

---
+++ example/lib/example.dart
@@ -0,0 +1,6 @@
+void main() {
+  /// The following line would normally show lint warnings
+  /// but we can disable the lint rules for this line using the following syntax:
+  // ignore: unused_local_variable, prefer_final_locals
+  var a = 5;
+}
---
+++ lib/netglade_analysis.dart
@@ -0,0 +1,3 @@
+/// NetGlade Dart and Flutter analyzer settings and best practices
+/// used internally at [NetGlade](https://netglade.cz/en).
+library netglade_analysis;
