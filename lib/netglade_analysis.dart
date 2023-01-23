52a45de0135022d97904b5b2b0cbb91fa6aff9e0
Honza Bittner <git@tenhobi.dev>
2023-01-23 13:12:17 +0100

    Fix links

--- lib/netglade_analysis.dart
+++ lib/netglade_analysis.dart
@@ -1,3 +1,3 @@
-/// NetGlade Dart and Flutter analyzer settings and best practices
-/// used internally at [NetGlade](https://netglade.cz/en).
+/// netglade Dart and Flutter analyzer settings and best practices
+/// used internally at [netglade](https://netglade.cz/en).
 library netglade_analysis;
