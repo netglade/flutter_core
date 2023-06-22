b1ba63594af682b385043b4e02fdcf7b5cdc2395
Honza Bittner <git@tenhobi.dev>
2023-06-22 13:59:27 +0200

    Check rules on internal projects

--- lib/netglade_analysis.dart
+++ lib/netglade_analysis.dart
@@ -1,3 +1,3 @@
-/// netglade Dart and Flutter analyzer settings and best practices
+/// NetGlade Dart and Flutter analyzer settings and best practices
 /// used internally at [netglade](https://netglade.cz/en).
 library netglade_analysis;
