d9b0e8b6b86391a660b61a9503f13b1a88cd9f19
Michal Ivičič <ivicic@netglade.cz>
2024-02-09 17:10:12 +0100

    + make toSet only implementational detail

--- packages/netglade_utils/lib/src/extensions/iterable_extensions.dart
+++ packages/netglade_utils/lib/src/extensions/iterable_extensions.dart
@@ -5,11 +5,11 @@ import 'package:collection/collection.dart';
 typedef Selector<T, K> = K Function(T selectFrom);
 
 extension IterableExtensions<T> on Iterable<T> {
-  Set<T> distinct() {
+  Iterable<T> distinct() {
     return toSet();
   }
 
-  /// Returns an iterable containing only elements from the given collection having distinct keys returned by the given [selector] function. 
+  /// Returns an iterable containing only elements from the given collection having distinct keys returned by the given [selector] function.
   /// If multiple elements have the same key, first element is returned.
   Iterable<T> distinctBy<K>(Selector<T, K> selector) {
     return groupBy<T, K>(this, selector)
