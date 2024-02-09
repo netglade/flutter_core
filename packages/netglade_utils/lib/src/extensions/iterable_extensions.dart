3d3618cd7f77fc8d9341578e5f6d1c2cb6ea42f6
Michal Ivičič <ivicic@netglade.cz>
2024-02-09 16:57:52 +0100

    + increase version, edit comments

--- packages/netglade_utils/lib/src/extensions/iterable_extensions.dart
+++ packages/netglade_utils/lib/src/extensions/iterable_extensions.dart
@@ -5,12 +5,12 @@ import 'package:collection/collection.dart';
 typedef Selector<T, K> = K Function(T selectFrom);
 
 extension IterableExtensions<T> on Iterable<T> {
-  /// Returns a set containing only distinct elements from the given collection.
   Set<T> distinct() {
     return toSet();
   }
 
-  /// Returns a list containing only elements from the given collection having distinct keys returned by the given [selector] function. If multiple elements have the same key, first element is returned.
+  /// Returns an iterable containing only elements from the given collection having distinct keys returned by the given [selector] function. 
+  /// If multiple elements have the same key, first element is returned.
   Iterable<T> distinctBy<K>(Selector<T, K> selector) {
     return groupBy<T, K>(this, selector)
         .values
